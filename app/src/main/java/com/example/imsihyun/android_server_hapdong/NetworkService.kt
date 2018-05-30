4307440e5fdb6a5a4ef60363b4461b9a23815698
ZZANZU <lees3301@gmail.com>
2018-05-31 03:10:38 +0900

    Fixed Login Error : url encoded로 보내야됨, @Part가 아니고 @Field임

--- app/src/main/java/com/example/imsihyun/android_server_hapdong/NetworkService.kt
+++ app/src/main/java/com/example/imsihyun/android_server_hapdong/NetworkService.kt
@@ -4,23 +4,21 @@ import com.example.imsihyun.android_server_hapdong.post.SigninResponse
 import com.example.imsihyun.android_server_hapdong.post.SignupResponse
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface NetworkService {
     // 회원가입
-    @Multipart
+    @FormUrlEncoded
     @POST("signup")
     fun postSignup (
-            @Part("user_id")    user_id        : RequestBody,
-            @Part("user_pw")    user_pwd       : RequestBody
+            @Field("user_id")    user_id        : RequestBody,
+            @Field("user_pw")    user_pwd       : RequestBody
     ) : Call<SignupResponse>
 
     // 로그인
     @POST("singin")
     fun postSignin (
-            @Part("user_id")    user_id         : RequestBody,
-            @Part("user_pw")    user_pwd        : RequestBody
+            @Field("user_id")    user_id         : RequestBody,
+            @Field("user_pw")    user_pwd        : RequestBody
     ) : Call<SigninResponse>
 }
