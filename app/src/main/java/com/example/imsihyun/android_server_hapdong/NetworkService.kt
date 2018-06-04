32aa45f9f263b0c6b30b3c42123b6454fa7caaa1
imsihyun <rightonbright@gmail.com>
2018-06-04 23:33:06 +0900

    Register network service

--- app/src/main/java/com/example/imsihyun/android_server_hapdong/NetworkService.kt
+++ app/src/main/java/com/example/imsihyun/android_server_hapdong/NetworkService.kt
@@ -1,7 +1,9 @@
 package com.example.imsihyun.android_server_hapdong
 
+import com.example.imsihyun.android_server_hapdong.post.RegisterResponse
 import com.example.imsihyun.android_server_hapdong.post.SigninResponse
 import com.example.imsihyun.android_server_hapdong.post.SignupResponse
+import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
@@ -21,5 +23,17 @@ interface NetworkService {
             @Field("user_id")    user_id         : RequestBody,
             @Field("user_pw")    user_pwd        : RequestBody
     ) : Call<SigninResponse>
-    ///
+
+    // 샵 등록하기
+    @Multipart
+    @POST("register/shop")
+    fun postRegister (
+            @Part("shop_name")     shop_name            : RequestBody,
+            @Part("shop_category") shop_category        : RequestBody,
+            @Part("shop_content")  shop_content         : RequestBody,
+            @Part registerImag                                : MultipartBody.Part?,
+            @Part("shop_info")     shop_info            : RequestBody,
+            @Part("menu_name")     menu_name            : RequestBody,
+            @Part("menu_price")    menu_price           : RequestBody
+    ) : Call<RegisterResponse>
 }
